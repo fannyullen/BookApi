e119d6d2fefb72d7ee4e7c6b4e037b7e33c1af90
Fanny Ullén <fanny.ullen@outlook.com>
2025-06-16 19:47:52 +0200

    Commit

--- obj/Debug/net9.0/BookApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0212dfeea713cc05f0fcb101183e56706bcfa25c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820a953c4ccb639f5e8d7f0501e5c6dc86bfa7b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
